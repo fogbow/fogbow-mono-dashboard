14f50b9fb7024c47767b37a83c152a0318ed744b
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-05-30 11:33:53 -0300

    Add menu for create instance.

---
+++ openstack_dashboard/dashboards/fogbow/dashboard.py
@@ -0,0 +1,16 @@
+from django.utils.translation import ugettext_lazy as _
+
+import horizon
+
+class MainPanel(horizon.PanelGroup):
+    slug = "fogbow-group"
+    name = _("User panel")
+    panels = ('members', 'request', 'instance', 'storage', 'network', 'attachment', 'usage', 'overview')       
+
+class Fogbow(horizon.Dashboard):
+    name = _("Federation")
+    slug = "fogbow"
+    panels = ( MainPanel, ) 
+    default_panel = 'members'
+
+horizon.register(Fogbow)
